6e401dff307f1c89f0c1a6f5ce8e95169023a54b
fnedarb <braden.fink49@gmail.com>
2021-12-03 01:58:28 -0500

    basic paths added

--- frontend_frdg/src/App.js
+++ frontend_frdg/src/App.js
@@ -1,5 +1,25 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from `react-router-dom`;
 import './App.css';
+
+import AddFood from `./components/AddFood`;
+import FoodList from `./components/FridgeShelves`;
+import FoodDetails from `./components/FoodDetails`;
+import UpdateFood from `./components/UpdateFood`;
+
+class App extends Component{
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component = {FoodList} />
+          <Route path = '/create-food' component = {AddFood} />
+          <Route path = '/edit-food:id' component = {UpdateFood} />
+        </div>
+      </Router>
+    )
+  }
+}
 
 function App() {
   return (
